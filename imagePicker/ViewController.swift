a8c1f4f93642750a4f97ba1e07b8ae930e7b02e7
AkashBelekar2428 <akkibelekar28@gmail.com>
2023-02-24 10:51:40 +0530

    imagePickera App Done

---
+++ imagePicker/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  imagePicker
+//  Created by Reapmind on 30/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+       
+    }
+
+    @IBAction func btnClicked(_ sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
+extension ViewController:UIImagePickerControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imageTap = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = imageTap
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
